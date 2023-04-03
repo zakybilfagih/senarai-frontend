dcf2248eefbedbb76c5ca7ef4ad3e675d90584d8
mackarelfish <mackarelfish93@gmail.com>
2023-04-03 17:51:50 +0700

    chore: lint

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './App.css'
 import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+const rootEl = document.getElementById('root')
+if (rootEl) {
+  const root = createRoot(rootEl)
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  )
+}
